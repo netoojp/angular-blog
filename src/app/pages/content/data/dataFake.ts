d059433235cc1d447bf643fb2135aafb6c5d807b
netoojp <jjoaopedronetoo@gmail.com>
2022-10-25 21:43:40 -0300

    finalização blog

---
+++ src/app/pages/content/data/dataFake.ts
@@ -0,0 +1,30 @@
+export const dataFake = [
+    {
+        "id": "1",
+        "title": "saiu nova postagem",
+        "description": "",
+        "photoCover": "https://webdescomplicada.com.br/wp-content/uploads/2022/01/Oqueeumblog.jpg"
+    },
+    {
+        "id": "2",
+        "title": "Novos Lançamentos Marvel CONFIRMADOS",
+        "description": "",
+        "photoCover": "https://d2d7ho1ae66ldi.cloudfront.net/ArquivoNoticias/7ec0067a-460f-11ec-aa6e-9587410378a2/marvel-disney-plus-shows.jpg"
+
+    },
+    {
+        "id": "3",
+        "title": "Jogos da fase de grupos CONFIRMADOS",
+        "description": "",
+        "photoCover": "https://files.tecnoblog.net/wp-content/uploads/2022/08/como-acompanhar-a-copa-do-mundo-de-2022-capa.png"
+
+    },
+    {
+        "id": "4",
+        "title": "Linguagens de Programação EM ALTA",
+        "description": "",
+        "photoCover": "https://www.tecnoveste.com.br/wp-content/uploads/2021/05/1_x8Fp9uOPgEaQdig7hV70qw.jpg"
+
+    }
+    
+]
